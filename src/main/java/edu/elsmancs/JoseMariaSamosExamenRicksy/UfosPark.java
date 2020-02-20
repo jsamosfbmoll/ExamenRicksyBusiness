e8a6e9dcb49ae19065c5206bad6dce0c81547034
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-02-20 17:57:08 +0100

    feat(UfosPark.java): Añadir método add y atributos a la clase
    Añadir método add que añade un ovni a flota (es un Hashmap)
    la llave como el string que representa al ovni y como valor
    la CreditCard del método no programado dispatch

--- src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/UfosPark.java
+++ src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/UfosPark.java
@@ -1,9 +1,17 @@
 package edu.elsmancs.JoseMariaSamosExamenRicksy;
 
+import java.util.HashMap;
+
 public class UfosPark implements GuestDispatcher {
 
+	private final double fee = 500;
+	private final HashMap<String, CreditCard> flota = new HashMap<String, CreditCard>();
+	
 	public void dispatch(CreditCard tarjetaDeCredito) {
 		
 	}
 
+	public void add(String ovni) {
+		flota.put(ovni, null);
+	}
 }
