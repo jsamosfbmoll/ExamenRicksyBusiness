f108a9f9140c47d51bc156d5dda34ce0f0d60323
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-02-20 20:23:27 +0100

    feat(RickMenu.java): Añadir constructor e implementación
    Añadir constructor de la clase e implementar la interfaz GuestDispatcher

--- src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/RickMenu.java
+++ src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/RickMenu.java
@@ -1,5 +1,17 @@
 package edu.elsmancs.JoseMariaSamosExamenRicksy;
 
-public class RickMenu {
+public class RickMenu implements GuestDispatcher {
+
+	private int stock = 0;
+	private final double itemCost;
+	
+	public RickMenu(int stock, double cost) {
+		this.stock = stock;
+		this.itemCost = cost;
+	}
+	
+	public void dispatch(CreditCard tarjetaDeCredito) {
+		
+	}
 
 }
