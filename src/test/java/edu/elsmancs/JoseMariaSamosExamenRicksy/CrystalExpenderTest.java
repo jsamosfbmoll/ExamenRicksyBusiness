fde691e7c2217a2926cacaefe792a889243e2a00
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-02-20 19:47:42 +0100

    test(CrystalExpenderTest.java): Añadir casos test sobre los getters
    Añadir casos test sobre los getters y el método dispatch
    implementado por la interfaz GuestDispatcher

---
+++ src/test/java/edu/elsmancs/JoseMariaSamosExamenRicksy/CrystalExpenderTest.java
@@ -0,0 +1,35 @@
+package edu.elsmancs.JoseMariaSamosExamenRicksy;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CrystalExpenderTest {
+
+	CrystalExpender cristal;
+	
+	@Before
+	public void setup() {
+		cristal = new CrystalExpender(5, 50.0);
+	}
+	
+	@Test
+	public void dispatch() {
+		CreditCard tarjeta = new CreditCard("Jose", "4321123443215678");
+		cristal.dispatch(tarjeta);;
+	}
+	
+	@Test
+	public void toString() {
+		assertEquals("stock: 5\ncost: 50.0", cristal.toString());
+	}
+	
+	@Test
+	public void stock() {
+		assertEquals(5, cristal.stock());
+		cristal.dispatch(new CreditCard("David", "3443211267765445"));
+		assertEquals(4, cristal.stock());
+	}
+
+}
