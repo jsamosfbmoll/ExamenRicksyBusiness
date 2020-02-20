210be9ead2b5991f02f88f97c6a4686bd85e3841
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-02-20 19:18:20 +0100

    feat(UfosPark.java): Añadir métodos a la clase UfosPark
    Añadir método que devuelve el ufo asignado a un número de tarjeta
    de crédito y programar método que asigna los números a los ufos.
    Se utilizan métodos privados para repartir responsabilidades entre los
    métodos

--- src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/UfosPark.java
+++ src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/UfosPark.java
@@ -5,10 +5,44 @@ import java.util.HashMap;
 public class UfosPark implements GuestDispatcher {
 
 	private final double fee = 500;
-	private final HashMap<String, CreditCard> flota = new HashMap<String, CreditCard>();
+	private final HashMap<String, String> flota = new HashMap<String, String>();
+	
+	private boolean ufosLibres() {
+		return flota.containsValue(null);
+	}
+	
+	private boolean ufoYaAlquilado(String numeroTarjeta) {
+		return flota.containsValue(numeroTarjeta);
+	}
+	
+	private void asignarUfo(String numeroTarjeta) {
+		for (String ovni : flota.keySet()) {
+			if (flota.get(ovni) == null) {
+				flota.put(ovni, numeroTarjeta);
+			}
+		}
+	}
 	
 	public void dispatch(CreditCard tarjetaDeCredito) {
-		
+		String numeroTarjeta = tarjetaDeCredito.number();
+		if (ufosLibres() && !ufoYaAlquilado(numeroTarjeta)) {
+			if (tarjetaDeCredito.pay(fee)) {
+				asignarUfo(numeroTarjeta);
+			}
+		}
+	}
+	
+	public String getUfoOf(String cardNumber) {
+		if (ufoYaAlquilado(cardNumber)) {
+			for (String ovni : flota.keySet()) {
+				if (flota.get(ovni) == cardNumber) {
+					return ovni;
+				}
+			}
+		} else {
+			return null;
+		}
+		return null;
 	}
 
 	public void add(String ovni) {
