05a7dd154d4b2e0cdfa8a9d9957908d95039810d
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-05-12 21:44:19 +0200

    docs(GestDispatcher.java): Añadir javadoc
    Añadir javadoc a la interfaz explicando su funcionamiento

--- src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/GuestDispatcher.java
+++ src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/GuestDispatcher.java
@@ -1,5 +1,13 @@
 package edu.elsmancs.JoseMariaSamosExamenRicksy;
 
+/**
+ * Interfaz que deben implementar los servicios de la fiesta
+ * para ser considerados como tal.
+ * 
+ * @author Jose María Samos Diago
+ *
+ */
+
 public interface GuestDispatcher {
 	
 	public void dispatch(CreditCard tarjetaDeCredito);
