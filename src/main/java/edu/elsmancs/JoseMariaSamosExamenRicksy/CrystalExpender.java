70657865d5cc7137b1b26fed5e13bb52a06e3887
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-05-12 22:14:30 +0200

    docs(CrystalExpender.java): Añadir javadoc
    Añadir javadoc a la clase, constructor y métodos necesarios

--- src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/CrystalExpender.java
+++ src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/CrystalExpender.java
@@ -1,20 +1,50 @@
 package edu.elsmancs.JoseMariaSamosExamenRicksy;
 
+/**
+ * Clase de tipo GuestDispatcher que maneja el expendedor
+ * de cristales para los invitados.
+ * 
+ * @author Jose María Samos Diago
+ * @see GuestDispatcher
+ *
+ */
+
 public class CrystalExpender implements GuestDispatcher {
 	
 	private int stock = 0;
 	private final double itemCost;
+	
+	/**
+	 * Constructor de la clase
+	 * 
+	 * @param stock Cantidad de cristales disponibles para la venta
+	 * @param cost Coste de cada cristal
+	 */
 	
 	public CrystalExpender(int stock, double cost) {
 		this.stock = stock;
 		this.itemCost = cost;
 	}
 	
+	/**
+	 * Cobra el cristal y reduce el stock ya que el cristal
+	 * vendido no esta disponible.
+	 * Método de la interfaz GuestDispatcher
+	 * 
+	 * @param tarjetaDeCredito Tarjeta que solicita el servicio
+	 * @see GuestDispatcher
+	 */
+	
 	public void dispatch(CreditCard tarjetaDeCredito) {
 		if (stock > 0 && tarjetaDeCredito.pay(itemCost)) {
 			stock -= 1;
 		}
 	}
+	
+	/**
+	 * 
+	 * @return Devuelve el stock de cristales dentro de la clase
+	 */
 	
 	public int stock() {
 		return stock;
