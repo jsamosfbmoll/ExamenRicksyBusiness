5c3243d7f1871b9cb7bb4e100755cd09040371f2
Jose Maria Samos <jsamos@cifpfbmoll.eu>
2020-05-12 21:36:30 +0200

    docs(CreditCard.java): Añadir javadoc
    Añadir javadoc a la clase, constructor y métodos.

--- src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/CreditCard.java
+++ src/main/java/edu/elsmancs/JoseMariaSamosExamenRicksy/CreditCard.java
@@ -1,5 +1,14 @@
 package edu.elsmancs.JoseMariaSamosExamenRicksy;
 
+/**
+ * Clase que representa una tarjeta de crédito que
+ * permite saber el crédito disponible y contiene funciones
+ * para el manejo del capital.
+ * 
+ * @author Jose María Samos Diago
+ *
+ */
+
 public class CreditCard {
 	
 	private final String owner;
@@ -7,18 +16,48 @@ public class CreditCard {
 	private double credit = 3000d;
 	private final String SYMBOL = "EZI";
 	
+	/**
+	 * Constructor de la clase en el que se asigna
+	 * el propietario y el número de la tarjeta.
+	 * 
+	 * Ambos campos son inmodificables.
+	 * 
+	 * @param nombreOwner Nombre del propietario
+	 * @param numeroTarjeta Número de la tarjeta
+	 */
+	
 	public CreditCard(String nombreOwner, String numeroTarjeta) {
 		this.owner = nombreOwner;
 		this.number = numeroTarjeta;
 	}
 	
+	/**
+	 * 
+	 * @return Este método devuelve el número de la tarjeta.
+	 */
+	
 	public String number() {
 		return this.number;
 	}
 	
+	/**
+	 * 
+	 * @return La cantidad de crédito disponible.
+	 */
+	
 	public double credit() {
 		return this.credit;
 	}
+	
+	/**
+	 * Permite el pago de un servicio restando la cantidad
+	 * de capital indicada y devuelve un valor booleano que indica
+	 * si la operación se ha completado con éxito o no.
+	 * 
+	 * @param ezi Cantidad de monedas a restar.
+	 * @return True si la transacción se ha completado satisfactoriamente,
+	 * false en caso contrario.
+	 */
 	
 	public boolean pay(double ezi) {
 		if (ezi > credit) {
